f9d86f22c6291314db413abfca13ef8cdf458fb8
RicharParker <61020823+RicharParker@users.noreply.github.com>
2020-05-15 10:29:05 -0500

    Update Menu_Opciones.c

--- Menu_Opciones.c
+++ Menu_Opciones.c
@@ -33,49 +33,45 @@ char MP(Persona array_de_personas[3])// MODIFICAR DATOS DE LA PERSONA
 	char resultado;
 	do
 	{
-		printf("\nSeleccione la persona a modificar [1-10]: ");
-		scanf("%d",&seleccion);
-		printf("\nPersona #%d\n",seleccion);
-		while ((m = getc(stdin)) != '\n' && m != EOF);
-		printf("Introduzca el nombre completo:\n");
-		fgets(array_de_personas[seleccion-1].nombre, 99,stdin);
-		printf("Introduzca el sexo [M/F]:\n");
-		array_de_personas[seleccion-1].sexo = getc(stdin);
-		while ((m = getc(stdin)) != '\n' && m != EOF);
-		printf("Introduzca la direccion:\n");
-		fgets(array_de_personas[seleccion-1].direccion, 99,stdin);
-		printf("Introduzca la religion:\n");
-		fgets(array_de_personas[seleccion-1].religion, 49,stdin);
-		printf("Introduzca la escolaridad:\n");
-		fgets(array_de_personas[seleccion-1].escolaridad, 19,stdin);
-		printf("Introduzca la edad:\n");
-		scanf("%hu", &array_de_personas[seleccion-1].edad);
-		while ((m = getc(stdin)) != '\n' && m != EOF);
-		printf("La persona #%d ha sido modificada\n",seleccion);
-		printf("Desea seguir modificando? [S/N]\n");
-		scanf("%c",&resultado);
+         printf("\nSeleccione la persona a modificar [1-10]: ");
+         scanf("%d",&seleccion);
+	 printf("\nPersona #%d\n",seleccion);
+	 while ((m = getc(stdin)) != '\n' && m != EOF);
+	 printf("Introduzca el nombre completo:\n");
+	 fgets(array_de_personas[seleccion-1].nombre, 99,stdin);
+	 printf("Introduzca el sexo [M/F]:\n");
+	 array_de_personas[seleccion-1].sexo = getc(stdin);
+	 while ((m = getc(stdin)) != '\n' && m != EOF);
+	 printf("Introduzca la direccion:\n");
+	 fgets(array_de_personas[seleccion-1].direccion, 99,stdin);
+	 printf("Introduzca la religion:\n");
+	 fgets(array_de_personas[seleccion-1].religion, 49,stdin);
+	 printf("Introduzca la escolaridad:\n");
+	 fgets(array_de_personas[seleccion-1].escolaridad, 19,stdin);
+	 printf("Introduzca la edad:\n");
+	 scanf("%hu", &array_de_personas[seleccion-1].edad);
+	 while ((m = getc(stdin)) != '\n' && m != EOF);
+	 printf("La persona #%d ha sido modificada\n",seleccion);
+	 printf("Desea seguir modificando? [S/N]\n");
+	 scanf("%c",&resultado);
 	}
 	while(resultado=='S'||resultado=='S');
 	return 0;
-}
-
-
-char SP(Persona array_de_personas[10]) //MOSTRAR DATOS DE LA PERSONA
-{
+        }
+       char SP(Persona array_de_personas[10]) //MOSTRAR DATOS DE LA PERSONA
+        {
 	for (size_t i = 0; i < 2; i++)
-
 	return 0;
-
-}
-char GD(Persona array_de_personas[10]) //Guardar datos
-{
+       }
+       char GD(Persona array_de_personas[10]) //Guardar datos
+       {
 	FILE *archivo;
 	printf("\nGuardando datos en archivo.bin\n");
 	archivo = fopen("archivo.bin", "wb");
 	if(archivo == NULL)
 	{
-		printf("Error al abrir \n");
-		return 1;
+	printf("Error al abrir \n");
+	return 1;
 	}
 	fwrite(array_de_personas, sizeof(Persona), 3, archivo);
 	printf("Guardado en file: archivo.bin\n");
@@ -108,45 +104,41 @@ int main(void)
         break;
 
         case 2:
-              system("cls");
-
-           FILE *archivo;
-           char cadena;
-	       archivo = fopen("datos.txt","rb");
-           while (feof(archivo)== 0){
-           fscanf(archivo, "%c",&cadena);
-           cadena= fgetc(archivo);
-           printf("%c", cadena);}
-           fclose(archivo);
-           break;
-           printf("\n\n Se ha leido el archivo correctamente");//
-           break;
-           case 3:
-				mostrar_personas(array_de_personas);
-				printf("\n\t\t\t\t¿Que desea hacer? \n");
-				printf("\t\t[1].Cargar datos de archivo\n");
-			    printf("\t\t[2].Modificar datos de una persona\n");
-				printf("\t\t[3].Guardar datos en archivo\n");
-				printf("\n\t\tIngrese su opcion: [ ]\b\b");
-				scanf("%d",&opc);
-
+         system("cls");
+         FILE *archivo;
+         char cadena;
+         archivo = fopen("datos.txt","rb");
+         while (feof(archivo)== 0){
+         fscanf(archivo, "%c",&cadena);
+         cadena= fgetc(archivo);
+         printf("%c", cadena);}
+         fclose(archivo);
          break;
-         case 4:
-				opc=0;
-				GD(array_de_personas);//GUARDA LOSS DATOS DE LA ARRAY
+         printf("\n\n Se ha leido el archivo correctamente");//
+         break;
+        case 3:
+         SP(array_de_personas);
+	 printf("\n\t\t\t\t¿Que desea hacer? \n");
+	 printf("\t\t[1].Cargar datos de archivo\n");
+         printf("\t\t[2].Modificar datos de una persona\n");
+	 printf("\t\t[3].Guardar datos en archivo\n");
+       	 printf("\n\t\tIngrese su opcion: [ ]\b\b");
+ 	 scanf("%d",&opc);
+         break;
+        case 4:
+	opc=0;
+	GD(array_de_personas);//GUARDA LOSS DATOS DE LA ARRAY
         break;
+        case 5:
+        break;
+        default:
+        system("cls");
+        printf("La opcion que ingreso es incorrecta");
+        getch();
+        break;
+        }
 
-
-      case 5:
-      break;
-      default:
-      system("cls");
-      printf("La opcion que ingreso es incorrecta");
-      getch();
-      break;
-      }
-
-      }while(opc != 5 );
+        }while(opc != 5 );
 
        return 0;
 }
